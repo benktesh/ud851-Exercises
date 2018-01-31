ce822176d6c2a6df6a368f5cd797437c32415ff7
Benktesh Sharma <benktesh@yahoo.com>
2018-01-31 12:34:07 -0600

    Updated to DONE Ex T05a.03

--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    // DONE (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
     static ArrayList<String> mLifecycleCallbacks = new ArrayList<String>();
 
     /**
@@ -74,14 +74,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        // DONE (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
         String data = "";
         for(int i = mLifecycleCallbacks.size()-1; i >= 0; i--){
             data = data + mLifecycleCallbacks.get(i) + "\n";
         }
         mLifecycleDisplay.setText(data);
         mLifecycleCallbacks.clear();
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
+        // DONE (5) Clear mLifecycleCallbacks after iterating through it
 
         logAndAppend(ON_CREATE);
     }
@@ -141,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        // DONE (2) Add the ON_STOP String to the front of mLifecycleCallbacks
         mLifecycleCallbacks.add(0, ON_STOP);
         logAndAppend(ON_STOP);
     }
@@ -168,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        // DONE (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
 
         mLifecycleCallbacks.add(0, ON_DESTROY);
         logAndAppend(ON_DESTROY);
